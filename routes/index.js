a7a7d479dbdd1faf0ce2ff967510e6328f65b0ca
Thompson Nkomo <nko20005@byui.edu>
2024-02-09 21:36:41 +0200

    commit

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,4 @@
 const router = require('express').Router();
-const contacts = require('./boards');
-const swagger = require('./swagger');
 
 // Main router middle ware, index route
 router.get('/', (req, res) => {
@@ -8,7 +6,7 @@ router.get('/', (req, res) => {
     res.send( "<center><h1 style='color:Green; padding-top: 10rem; font-size:4rem'>Welcome to my CSE341 Boards API page</h1></center>");
 })
 
-router.use('/', require('./swagger'));
+router.use('/swagger', require('./swagger'));
 
 router.use('/boards', require('./boards'));
 
